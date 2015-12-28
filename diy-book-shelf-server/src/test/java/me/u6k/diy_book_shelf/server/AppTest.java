1aa1d80b90b86d08af36c1e058204a023a8db06a
Naono Yuichi <u6k.apps@gmail.com>
2015-12-28 12:56:07 +0900

    "pom.xml"をSpringBoot用に修正して、AppクラスをSpringBoot起動用に修正した。

---
+++ diy-book-shelf-server/src/test/java/me/u6k/diy_book_shelf/server/AppTest.java
@@ -0,0 +1,20 @@
+
+package me.u6k.diy_book_shelf.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
